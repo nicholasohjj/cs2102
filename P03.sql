970a5d7f64e40c5cc4a7f00fe22fa7159de9815c
Nicholas Oh <nicholasohjj@gmail.com>
2024-03-31 21:15:12 +0800

    .p03

---
+++ P03.sql
@@ -0,0 +1,95 @@
+/*
+Group #
+1. Name 1
+  - Contribution A
+  - Contribution B
+2. Name 2
+  - Contribution A
+  - Contribution B
+*/
+
+
+
+/* Write your Trigger Below */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+  Write your Routines Below
+    Comment out your routine if you cannot complete
+    the routine.
+    If any of your routine causes error (even those
+    that are incomplete), you may get 0 mark for P03.
+*/
+
+-- PROCEDURE 1
+CREATE OR REPLACE PROCEDURE add_employees (
+  eids INT[], enames TEXT[], ephones INT[], zips INT[], pdvls TEXT[]
+) AS $$
+-- add declarations here
+BEGIN
+  -- your code here
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- PROCEDURE 2
+CREATE OR REPLACE PROCEDURE add_car (
+  brand   TEXT   , model  TEXT   , capacity INT  ,
+  deposit NUMERIC, daily  NUMERIC,
+  plates  TEXT[] , colors TEXT[] , pyears   INT[], zips INT[]
+) AS $$
+-- add declarations here
+BEGIN
+  -- your code here
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- PROCEDURE 3
+CREATE OR REPLACE PROCEDURE return_car (
+  bid INT, eid INT
+) AS $$
+-- add declarations here
+BEGIN
+  -- your code here
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- PROCEDURE 4
+CREATE OR REPLACE PROCEDURE auto_assign () AS $$
+-- add declarations here
+BEGIN
+  -- your code here
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- FUNCTION 1
+CREATE OR REPLACE FUNCTION compute_revenue (
+  sdate DATE, edate DATE
+) RETURNS NUMERIC AS $$
+  -- your code here
+$$ LANGUAGE plpgsql;
+
+
+-- FUNCTION 2
+CREATE OR REPLACE FUNCTION top_n_location (
+  n INT, sdate DATE, edate DATE
+) RETURNS TABLE(lname TEXT, revenue NUMERIC, rank INT) AS $$
+  -- your code here
+$$ LANGUAGE plpgsql;
